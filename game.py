b80c0a20405ddc63b4f0e2dd521e61c0c58e7770
nrnbenn <125394324+nrnbenn@users.noreply.github.com>
2024-02-02 09:34:04 +0000

    added Game Loop and Sreenshot Function
    Main setup of game class finished

--- game.py
+++ game.py
@@ -3,11 +3,25 @@ import pygame
 class game():
 	def __init__(self):
 		self.PygameSetup()
+		self.GameLoop()
+		
 
 	def PygameSetup(self):
 		pygame.init()
 		self.DISPLAYSURF = pygame.display.set_mode((400,300))
 		pygame.display.set_caption('X-Wing')
+
+	def GameLoop(self):
+		#loop
+		while True:
+			for event in pygame.event.get():
+				if event.type == 'QUIT':
+					pygame.quit()
+					sys.exit()
+					pygame.display.update()
+
+	def screenshot(self):
+		pygame.image.save(self.DISPLAYSURF, "shots/screenshot.jpg")
 
 class mover():
 	pass
